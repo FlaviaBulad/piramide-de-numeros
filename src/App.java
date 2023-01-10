845a9c54eeb41afaff71eb085180ef25378370f4
Flavia <flavia.bulad@gmail.com>
2023-01-10 14:58:37 -0300

    switch language to pt-br

--- src/App.java
+++ src/App.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Enter a number:");
+        System.out.println("Digite um numero");
         Scanner input = new Scanner(System.in);
         int number = input.nextInt();
        input.close();
 
-        System.out.println("Number pyramid starting from 1 to " + number + "\n");
+        System.out.println("Piramide de numeros do 1 ao " + number + "\n");
         for (int i = 1; i <= number; i++) {
             for (int j = 0; j < i; j++) {
                 System.out.print(i);
