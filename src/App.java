39c4ba82a558a5fc64b528ee5c781472a3caa66c
Flavia <flavia.bulad@gmail.com>
2023-01-10 14:49:36 -0300

    add solution

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Enter a number:");
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+       input.close();
+
+        System.out.println("Number pyramid starting from 1 to " + number + "\n");
+        for (int i = 1; i <= number; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
     }
 }
